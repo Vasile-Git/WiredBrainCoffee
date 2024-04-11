800f125c03eb3a37c380a789e60dc0c7046930f3
Vasile <vasile.gaspar@technosoft.nl>
2024-04-11 15:25:17 +0200

    module 5 - Organize code with UserControls

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/Controls/HeaderControl.xaml.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/Controls/HeaderControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WiredBrainCoffee.CustomersApp
+namespace WiredBrainCoffee.CustomersApp.Controls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HeaderControl.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HeaderControl : UserControl
     {
-        public MainWindow()
+        public HeaderControl()
         {
             InitializeComponent();
         }
-
     }
 }
