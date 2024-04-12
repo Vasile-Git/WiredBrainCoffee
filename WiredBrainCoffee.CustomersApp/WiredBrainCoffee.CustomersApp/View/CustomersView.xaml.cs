864d093375a5fa42b6605924db90f24c143e5369
Vasile <vasile.gaspar@technosoft.nl>
2024-04-12 23:31:57 +0200

    module 6 - Applying Data Binding and MVVM
    - Create data bindings in XAML
      - Bind to another element
      - Know how the data context works
    
     - Apply the MVVM pattern
    
    - Work with data bindings
      - Notify about property changes
      - Use the XAML Binding Failures window
      - Convert values with an IValueConverter

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp.View
 {
@@ -8,22 +10,29 @@ namespace WiredBrainCoffee.CustomersApp.View
     /// </summary>
     public partial class CustomersView : UserControl
     {
+        private CustomersViewModel _viewModel;
+
         public CustomersView()
         {
             InitializeComponent();
+            _viewModel = new CustomersViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CustomersView_Loaded;
+        }
+
+        private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
         }
 
         private void ButtonNavigation_Click(object sender, RoutedEventArgs e)
         {
-            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            _viewModel.MoveNavigation();
+        }
 
-            //var newColumn = column == 0 ? 2 : 0;
-            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
-
-            var column = Grid.GetColumn(customerListGrid);
-
-            var newColumn = column == 0 ? 2 : 0;
-            Grid.SetColumn(customerListGrid, newColumn);
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Add();
         }
     }
 }
---
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.ObjectModel;
+using System.Data.Common;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class CustomersViewModel : ViewModelBase
+    {
+        private readonly ICustomerDataProvider _customerDataProvider;
+        private CustomerItemViewModel? _selectedCustomer;
+        private NavigationSide _navigationSideColumn;
+
+        public CustomersViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            _customerDataProvider = customerDataProvider;
+        }
+        public ObservableCollection<CustomerItemViewModel> Customers { get; } = new();
+
+        public CustomerItemViewModel? SelectedCustomer
+        {
+            get => _selectedCustomer;
+            set
+            {
+                _selectedCustomer = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public NavigationSide NavigationSideColumn
+        {
+            get => _navigationSideColumn;
+            private set
+            {
+                _navigationSideColumn = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public async Task LoadAsync()
+        {
+            if (Customers.Any())
+            {
+                return;
+            }
+
+            var customers = await _customerDataProvider.GetAllAsync();
+            if (customers is not null)
+            {
+                foreach (var customer in customers)
+                {
+                    Customers.Add(new CustomerItemViewModel(customer));
+                }
+            }
+        }
+
+        internal void Add()
+        {
+            var customer = new Customer { FirstName = "New" };
+            var viewModel = new CustomerItemViewModel(customer);
+            Customers.Add(viewModel);
+            SelectedCustomer = viewModel;
+        }
+
+        internal void MoveNavigation()
+        {
+            NavigationSideColumn = NavigationSideColumn == NavigationSide.Left
+                ? NavigationSide.Right
+                : NavigationSide.Left; 
+        }
+
+        public enum NavigationSide
+        {
+            Left,
+            Right
+        }
+    }
+}
