01c154f3495b01d2984e4f779354d8797ebbc13a
Vasile <vasea.gaspar@gmail.com>
2024-05-22 16:31:43 +0200

    module 9 - Working with Data Templates
    part 3 - was implemented the logic to swap the views in the MainWindow with the ContentControl (<ContentControl Grid.Row="2" Content="{Binding SelectedViewModel}"/>) and with 2 implicite DataTemplate

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/MainViewModel.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/MainViewModel.cs
@@ -3,13 +3,15 @@
     public class MainViewModel : ViewModelBase
     {
         private readonly CustomersViewModel _customersViewModel;
+        private readonly ProductsViewModel _productsViewModel;
         private ViewModelBase? _selectedViewModel;
 
-        public MainViewModel(CustomersViewModel customersViewModel)
+        public MainViewModel(CustomersViewModel customersViewModel,
+            ProductsViewModel productsViewModel)
         {
             _customersViewModel = customersViewModel;
+            _productsViewModel = productsViewModel;
             SelectedViewModel = _customersViewModel;
-
         }
 
         public ViewModelBase SelectedViewModel
