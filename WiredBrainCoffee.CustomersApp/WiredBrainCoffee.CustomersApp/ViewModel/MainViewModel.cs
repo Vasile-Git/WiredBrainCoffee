4b06bf0a49a940d5c89ea18bea057fad491feb4f
Vasile <vasea.gaspar@gmail.com>
2024-05-22 17:10:26 +0200

    module 9 - Working with Data Templates
    part 4 - Create a SelectViewModelCommand and Bind the menu items to the command;
    
    module Summary:
    - WPF's flexible content model
    	- ContentControl and ItemsControl;
    	- UIElements are rendered;
    	- For other objects ToString result is shown;
    - Create and use data templates
    	- Explicit data template with x:Key;
    	- Implicit data template with DataType;
    - Use data templates to load detail views

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/MainViewModel.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/MainViewModel.cs
@@ -1,20 +1,21 @@
-﻿namespace WiredBrainCoffee.CustomersApp.ViewModel
+﻿using WiredBrainCoffee.CustomersApp.Command;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        private readonly CustomersViewModel _customersViewModel;
-        private readonly ProductsViewModel _productsViewModel;
         private ViewModelBase? _selectedViewModel;
 
         public MainViewModel(CustomersViewModel customersViewModel,
             ProductsViewModel productsViewModel)
         {
-            _customersViewModel = customersViewModel;
-            _productsViewModel = productsViewModel;
-            SelectedViewModel = _customersViewModel;
+            CustomersViewModel = customersViewModel;
+            ProductsViewModel = productsViewModel;
+            SelectedViewModel = CustomersViewModel;
+            SelectViewModelCommand = new DelegateCommand(SelectViewModel);
         }
 
-        public ViewModelBase SelectedViewModel
+        public ViewModelBase? SelectedViewModel
         {
             get => _selectedViewModel;
             set
@@ -24,12 +25,23 @@
             }
         }
 
+        public CustomersViewModel CustomersViewModel { get; }
+        public ProductsViewModel ProductsViewModel { get; }
+
+        public DelegateCommand SelectViewModelCommand{ get; }
+
         public async override Task LoadAsync()
         {
             if (SelectedViewModel is not null)
             {
                 await SelectedViewModel.LoadAsync();
             }
+        }
+
+        private async void SelectViewModel(object? parameter)
+        {
+            SelectedViewModel = parameter as ViewModelBase;
+            await LoadAsync();
         }
     }
 }
