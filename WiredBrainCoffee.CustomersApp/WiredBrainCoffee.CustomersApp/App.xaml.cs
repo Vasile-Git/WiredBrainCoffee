a1a4cca399ab23a142c19e3308fcc584dae662a3
Vasile <vasea.gaspar@gmail.com>
2024-05-24 16:09:42 +0200

    module 10 - Setting up Dependency Injection (manually injected dependencies)  - The MainViewModel itself has the CustomerViewModel and the ProductsViewModel as dependencies (so actually, all the constructor parameters are dependencies that you inject) as manually injected dependencies (by calling the constructors).

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/App.xaml.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/App.xaml.cs
@@ -1,14 +1,20 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow(new MainViewModel(
+                new CustomersViewModel(new CustomerDataProvider()),
+                new ProductsViewModel()));
+            mainWindow.Show();
+        }
     }
 
 }
