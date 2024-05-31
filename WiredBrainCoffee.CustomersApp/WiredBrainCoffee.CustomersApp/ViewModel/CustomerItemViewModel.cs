a1b6ed2cb45c089864a6868922aa15e462300f1b
Vasile <vasea.gaspar@gmail.com>
2024-05-31 15:51:35 +0200

    module 13 - Validating User Input
    - Validate the FirstName Property;
    - In the AddError() and ClearErrors() methods has been added the "CallerMemberName" attribute so that the compiler automatically passes the name of the property ...
    Because of that the arguments ("nameof(FirstName)") from the caller methods are not necessary and the compiler automatically passes the caller member name to the methods:
    
    AddError("FirstName is required", nameof(FirstName)); ->
    AddError("FirstName is required");

--- WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/CustomerItemViewModel.cs
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/CustomerItemViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
-    public class CustomerItemViewModel : ViewModelBase
+    public class CustomerItemViewModel : ValidationViewModelBase
     {
         private readonly Customer _model;
 
@@ -10,7 +10,7 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
         {
             _model = model;
         }
-
+         
         public int Id => _model.Id;
 
         public string? FirstName
@@ -20,6 +20,14 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
             {
                 _model.FirstName = value;
                 RaisePropertyChanged();
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    AddError("FirstName is required");
+                }
+                else
+                {
+                    ClearErrors();
+                }
             }
         }
 
